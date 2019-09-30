9cebe0c7eb900fb14891008ab1f295da7fbc1a72
holmesdustin <holmes_dustin@columbusstate.edu>
2019-09-30 23:17:55 +0000

    currentcomic

--- content/comic.php
+++ content/comic.php
@@ -1,3 +1,2 @@
 <div id="randomPic"><?php getRandomComic(); ?></div>
-<br><br>
-<button id="button" class="btn btn-outline-primary">Get Random Comic</button>
+<button id="button" class="btn btn-outline-primary">New Comic</button>
